658e305e763013f1ac997956af8889cbd27f6126
Alan Córdoba <alan.cordoba@gmail.com>
2024-08-13 16:30:12 -0300

    Update .config/nvim/lua/acordoba/plugins/nvim-tree.lua

--- dot_config/nvim/lua/acordoba/plugins/nvim-tree.lua
+++ dot_config/nvim/lua/acordoba/plugins/nvim-tree.lua
@@ -7,6 +7,7 @@ return {
     -- recommended settings from nvim-tree documentation
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
+    vim.opt.termguicolors = true
 
     nvimtree.setup({
       view = {
@@ -52,5 +53,5 @@ return {
     keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
-  end
+  end,
 }
