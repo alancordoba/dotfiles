b9036b39da0389071d3c8a7ee207e44437595afe
Alan Córdoba <alan.cordoba@gmail.com>
2024-09-01 11:27:04 -0300

    Update .config/nvim/lua/acordoba/plugins/catppuccin.lua

--- dot_config/nvim/lua/acordoba/plugins/catppuccin.lua
+++ dot_config/nvim/lua/acordoba/plugins/catppuccin.lua
@@ -16,24 +16,24 @@ return {
         functions = { "italic" },
         types = { "bold" },
       },
-      --color_overrides = {
-      --  mocha = {
-      --    base = "#171717", -- background
-      --    surface2 = "#9A9A9A", -- comments
-      --    text = "#F6F6F6",
-      --  },
-      --},
-      --highlight_overrides = {
-      --  mocha = function(C)
-      --    return {
-      --      NvimTreeNormal = { bg = C.none },
-      --      CmpBorder = { fg = C.surface2 },
-      --      Pmenu = { bg = C.none },
-      --      NormalFloat = { bg = C.none },
-      --      TelescopeBorder = { link = "FloatBorder" },
-      --    }
-      --  end,
-      --},
+      color_overrides = {
+        mocha = {
+          base = "#171717", -- background
+          surface2 = "#9A9A9A", -- comments
+          text = "#F6F6F6",
+        },
+      },
+      highlight_overrides = {
+        mocha = function(C)
+          return {
+            NvimTreeNormal = { bg = C.none },
+            CmpBorder = { fg = C.surface2 },
+            Pmenu = { bg = C.none },
+            NormalFloat = { bg = C.none },
+            TelescopeBorder = { link = "FloatBorder" },
+          }
+        end,
+      },
       integrations = {
         barbar = true,
         cmp = true,
